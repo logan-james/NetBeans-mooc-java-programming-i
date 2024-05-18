a2512bee75e9570e05330fde93aa8202c5b6b28f
logan-james <loganjames448@gmail.com>
2024-05-17 17:03:29 -0700

    Using NetBeans cause vscode tmc extention took a shit.

---
+++ part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
@@ -0,0 +1,16 @@
+
+public class YourFirstAccount {
+
+    public static void main(String[] args) {
+        // Do not touch the code in Account.java
+        // Write your program here
+        
+        Account Logan = new Account("Logan", 100);
+        
+        Logan.deposit(20.0);
+        
+        System.out.println(Logan);
+    }
+}
+
+
